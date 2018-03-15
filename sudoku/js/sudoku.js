7cce29e838125ca287b6e2bee701e78ff5017612
Harshit Agrawal <harshitagrawal.iitr@gmail.com>
2018-03-15 18:22:36 +0530

    sudoku | tried adding sudoku generator

--- sudoku/js/sudoku.js
+++ sudoku/js/sudoku.js
@@ -7,9 +7,52 @@ function assignSudoku(sudoku){
       var cell = data_cells[i*9 + j];
       cell.value = null; cell.fixed = false; cell.selected = false; cell.highlighted = false;
       if(sudoku[i][j] != 0){ cell.value = sudoku[i][j]; cell.fixed = true; }
+      // if(sudoku[i*9 + j].value != 0){ cell.value = sudoku[i*9 + j].value; cell.fixed = true; }
     }
   }
   update_Cells();
+}
+
+// ************************************************************************************** //
+// Create a sudoku
+
+function createSudoku(){
+
+  sudoku_arr = [];
+  for(var i = 1; i <= 9; i++){
+    for(var j = 1; j <= 9; j++){
+      var temp_x_pos = parseInt((i-1)/3);
+      var temp_y_pos = parseInt((j-1)/3);
+      var temp_square_pos = temp_x_pos*3 + temp_y_pos;
+      var temp_id = (i-1)*9 + j;
+
+      var temp_object = { x: i, y: j, square: temp_square_pos, id: temp_id, value: 0 };
+      sudoku_arr.push(temp_object);
+    }
+  }
+
+  rows = d3.range(10).map(d => { return([]) });
+  columns = d3.range(10).map(d => { return([]) });
+  squares = d3.range(10).map(d => { return([]) });
+
+  var temp_val = null;
+  for(var i = 0; i < sudoku_arr.length; i++){
+    temp_val = math.randomInt(1,10);
+    sudoku_arr[i].value = assignValue(sudoku_arr[i], temp_val);
+  }
+
+  function assignValue(cell, value){
+    if( rows[cell.x].indexOf(value) == -1 && columns[cell.y].indexOf(value) == -1 && squares[cell.square].indexOf(value) == -1 ){
+      rows[cell.x].push(value); columns[cell.y].push(value); squares[cell.square].push(value);
+      return value
+    } else {
+      console.log(value);
+      value++; if(value == 10){ value = 1; }
+      return( assignValue(cell, value) );
+    }
+  }
+
+  assignSudoku(sudoku_arr);
 }
 
 // ************************************************************************************** //
