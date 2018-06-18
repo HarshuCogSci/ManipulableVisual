a7d65a875b2711d33986475b90842bcf685121bb
RISHABH <dubeyrishabh.rd@gmail.com>
2018-06-18 15:25:29 +0530

    changes made

---
+++ buffons_needle/result.js
@@ -0,0 +1,13 @@
+function result(){
+
+var x=document.getElementById("r1");
+x.innerHTML="Total no. of needeles dropped : "+a;
+
+var y=document.getElementById("r2");
+y.innerHTML="Total intersection : "+m;
+
+var z=document.getElementById("r3");
+z.innerHTML="&#928 &#8776  " +q;
+    
+updatePlot();
+}
